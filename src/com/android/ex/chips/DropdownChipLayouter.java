03fda65124d0949633b7db000357aa0ed7bd5072
Vinay Verma <vvinay@codeaurora.org>
2015-05-25 18:58:52 +0530

    Add check for ArrayOutofBoundException
    Added check to handle ArrayOutofBoundException when Rfc Tokenizer
    is returning zero elements for unexpected  message recipients.
    
    Change-Id: I5a08fe2d9e59cc2ed8e0780dedef4cbc6c8f7060

--- src/com/android/ex/chips/DropdownChipLayouter.java
+++ src/com/android/ex/chips/DropdownChipLayouter.java
@@ -9,6 +9,7 @@ import android.support.annotation.DrawableRes;
 import android.support.annotation.IdRes;
 import android.support.annotation.LayoutRes;
 import android.text.TextUtils;
+import android.text.util.Rfc822Token;
 import android.text.util.Rfc822Tokenizer;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -119,7 +120,12 @@ public class DropdownChipLayouter {
                 }
                 break;
             case SINGLE_RECIPIENT:
-                destination = Rfc822Tokenizer.tokenize(entry.getDestination())[0].getAddress();
+                Rfc822Token[] rFCTokens = Rfc822Tokenizer.tokenize(entry.getDestination());
+                if (rFCTokens.length == 0) {
+                    destination = entry.getDestination();
+                } else {
+                    destination = rFCTokens[0].getAddress();
+                }
                 destinationType = null;
         }
 
